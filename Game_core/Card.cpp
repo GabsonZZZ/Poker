08b1e84d9a1231e9240bc009408869a6dc0e3e89
Aleksandra02K <101323447+Aleksandra02K@users.noreply.github.com>
2022-06-01 22:55:19 +0200

    Update Card.cpp

--- Game_core/Card.cpp
+++ Game_core/Card.cpp
@@ -5,3 +5,6 @@ Class::Class(const int value, const in suit) : value_(value), suit_(suit) {
 	assert(suit_ == HEARTS || suit_ == CLUBS || suit_ == DIAMONDS || suit_ == SPADES);
 	assert(value_ >= LOWEST_CARD_VALUE && value_ <= HIGHEST_CARD_VALUE);
 }
+
+
+string Card::ToString(const bool compressed)
