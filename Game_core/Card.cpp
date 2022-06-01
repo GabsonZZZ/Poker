2720b6e40a06fc9a9c0baf500763c20263274fc6
Aleksandra02K <101323447+Aleksandra02K@users.noreply.github.com>
2022-06-01 23:00:05 +0200

    Update Card.cpp

--- Game_core/Card.cpp
+++ Game_core/Card.cpp
@@ -10,5 +10,7 @@ Class::Class(const int value, const in suit) : value_(value), suit_(suit) {
 string Card::ToString(const bool compressed) const {
 	if (compressed)
 		return ToShortString();
+	else
+		return ToLongString();
 }
 
