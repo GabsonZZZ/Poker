183a8d7e204a4d27c99990884099c8885f7b7003
GabsonZZZ <102111162+GabsonZZZ@users.noreply.github.com>
2022-06-02 15:32:44 +0000

    dyrektywy preprocesora, biblioteki

--- Game_core/Card.h
+++ Game_core/Card.h
@@ -1,3 +1,6 @@
+#ifndef CARD_H_
+#define CARD_H_
+
 enum SUIT {
     CLUBS = 0,
     DIAMONDS = 1,
@@ -5,9 +8,13 @@ enum SUIT {
     SPADES = 3
 };
 
+#include <string>
+
 enum FACE_CARDS {
     JACK = 0,
     QUEEN = 1,
     KING = 2,
     ACE = 3
 };
+
+#endif
