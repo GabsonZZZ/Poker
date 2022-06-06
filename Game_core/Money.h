0ed9235fc6700f9e364827d54bb28eda2d554e23
GabsonZZZ <102111162+GabsonZZZ@users.noreply.github.com>
2022-06-06 20:27:15 +0000

    dyrektywy, dokumentacja money

---
+++ Game_core/Money.h
@@ -0,0 +1,9 @@
+/*
+*plik money.h definiuje walute używaną w grze, która ma wspomóc mechanikę stawiania *zakładu w poszczególnej rundzie. Implementuje także nasz aktualny stan ilości żetonów, *które posiadamy.
+*/
+
+#ifndef MONEY_H_
+#define MONEY_H_
+
+
+#endif 
