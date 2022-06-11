42e7eb1259655ae8a9bd2fc591d552ef07566cd5
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2022-06-11 23:16:12 +0200

    play
    ¿? KOMENTARZE POLECAM ¿?

--- functions.py
+++ functions.py
@@ -7,4 +7,17 @@ def fold(player: Player, current_deck: List[Card]) -> None:            #gracz pa
     del player.cards[-2]
     deck.deck_update(current_deck.cards)
 
+    
+b_blind = 50                                                                   #to jest do zmiany, np. zeby pytac sie na poczatku gracza, jakie stawki chce
+
+def play(player: Player, current_bet: int, balance: int) -> int:               #gracz gra (przebicie, tak?)
+    player.bet = int(input("Wybierz kwotę, którą chcesz zagrać: "))            #jaką kwotą
+    if bet > balance or bet < 2 * b_blind or bet <= current_bet:               #jesli da za malo albo za duzo to jest zle
+        raise ValueError
+    balance -= player.bet                                                      #ta kwota jest mu zabierana z konta tak jakby
+    return player.bet                                                          #zwraca bet (zapamietuje), zeby moc go potem podpisac pod last_bet, zeby call mial z czego korzystac, tj. last_bet = play(...)
+
+### do klasy Player przydałoby się self.balance i funkcje increase/decrease tego typu:
+###     def decrease_balance(self, change):     
+###          self.balance -= change
 
