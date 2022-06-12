dba5f02ea01f05045cfe8048d10b7545c01ff9c8
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2022-06-12 21:17:19 +0200

    royal flush

--- new_functions.py
+++ new_functions.py
@@ -19,3 +19,20 @@ def choose_cards(player: Player, current_flop: List[Card]) -> List[Card]: #zwrac
         print("Wybrał*ś już 5 kart!")
     return cards
 
+
+def royal_flush(cards: List[Card], player: Player) -> None:
+    score = 10                              #do zmiany moze byc ta ilosc punktow
+    card1 = cards[0]
+    if len(cards) != 5:
+        return None
+    for card in cards:
+        if card.suit != card1.suit:
+            return None
+    for card in cards:
+        if not (card.value == 10 or card.value == 11 or card.value == 12 or card.value == 13 or card.value == 14) and (card != card1):
+            return None
+    print("Royal flush! Zyskujesz {} punktow".format(score))
+    player.new_score(score)
+        #funckja w klasie ktora dodaje punkty
+        ### w klasie Player przydałoby sie pole score - ile gracz ma aktualnie punktów
+
