961cc3aefcb4dcc41a82830328722c30a58202e1
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2022-06-12 21:16:13 +0200

    pomocnicza
    pewnie do wywalenia/edycji

--- new_functions.py
+++ new_functions.py
@@ -1,1 +1,21 @@
+#### punktacja
+### tutaj funkcja, w ktorej gracz wybiera karty do swojej kombinacji ###
+
+def choose_cards(player: Player, current_flop: List[Card]) -> List[Card]: #zwraca liste kart potrzebna do kolejnej funkcji
+    cards = []
+    i = 1
+    for card in player.cards:                   #wybieranie ze swoich kart
+        print("Chcesz użyć karty {}?(wpisz {})".format(i, i))
+        if card == i:
+            cards.append(card)
+        i += 1
+    j = 1
+    for card in current_flop:                   #wybieranie z flopa
+        print("Chcesz użyć karty {}?(wpisz {})".format(j, j))
+        if len(cards) <= 5:
+            if card == j:
+                cards.append(card)
+            j += 1
+        print("Wybrał*ś już 5 kart!")
+    return cards
 
