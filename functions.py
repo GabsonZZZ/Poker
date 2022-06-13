8716c30aeb66b8cd7ccf22f8d67f3ebf577bb58b
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2022-06-13 20:16:00 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,8 +6,7 @@ def fold(player: Player, current_deck: List[Card]) -> None:            #gracz pa
     del player.cards[-1]
     current_deck.append(player.cards[-2])
     del player.cards[-2]
-    deck.deck_update(current_deck.cards)
-
+    
     
 b_blind = 50                                                                   #to jest do zmiany, np. zeby pytac sie na poczatku gracza, jakie stawki chce
 
