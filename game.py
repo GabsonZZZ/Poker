fef4f622b5044eed7e56075150afe2cc60400c15
Aleksandra02K <101323447+Aleksandra02K@users.noreply.github.com>
2022-06-14 12:20:20 +0200

    Update game.py

--- game.py
+++ game.py
@@ -16,6 +16,29 @@ def pair_or_three_of_a_kind(self, cards: [Card], symbols):
         if len(cards) != 5:
             return False, 0;
         value_dict = dict([(x,0) for x in symbols])
-            for die in cards:
-                  value_dict[die.symbol] += 1
-            set_dict = dict([(x, 0) for x in range(0, 6)])
+        for die in cards:
+            value_dict[die.symbol] += 1
+        set_dict = dict([(x, 0) for x in range(0, 6)])
+        for set_size in value_dict.values():
+            set_dict[set_size] += 1
+        if set_dict[2] == 1 and set_dict[3] != 1 and set_dict[4] != 1:
+            print("Pair! Otrzymujesz {} punktów.".format(score))
+            return True, score;                     #do zmiany po ustaleniu wartości score
+        elif set_dict[3] == 1 and set_dict[4] != 1:
+            score = 400
+            print("Three of a kind! Otrzymujesz {} punktów.".format(score))
+            return True, score;
+        elif set_dict[4] == 1:
+            score = 700
+            print("Four of a kind! Otrzymujesz {} punktów.".format(score))
+            return True, score;
+        elif set_dict[2] == 1 and set_dict[3] == 1:
+            score = 600
+            print("Full house! Otrzymujesz {} punktów.".format(score))
+            return True, score;
+        elif set_dict[2] == 2:
+            score = 300
+            print("Two pair! Otrzymujesz {} punktów.".format(score))
+            return True, score;
+        else:
+            return False, 0;
