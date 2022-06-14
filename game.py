02d2add09f03e2a09b603add285b8cc9de2fb11e
Aleksandra02K <101323447+Aleksandra02K@users.noreply.github.com>
2022-06-14 12:12:27 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,5 @@
 def stack_creation(self):
+ values = {"A": 14, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12,
+                  "K": 13}
+        suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+        suits_values = {"Spades": "\u2660", "Hearts": "\u2665", "Clubs": "\u2663", "Diamonds": "\u2666"}
