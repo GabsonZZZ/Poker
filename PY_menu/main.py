e1e6390f59004e847d5d5c14ba72fd1977455a39
KaczMac <100795809+KaczMac@users.noreply.github.com>
2022-06-14 12:35:03 +0200

    Update main.py

--- PY_menu/main.py
+++ PY_menu/main.py
@@ -21,3 +21,45 @@ def move(Gra):
             break
         else:
             print("Zła komenda, proszę spróbować ponownie.")
+
+def main():
+    n = pulpit.start_pulpit() #Odebranie z funkcji start_pulpit zmiennych (Lista graczy , liczba talii)
+    Gra = game.Game(n[0],int(n[1])) #Inicjacja klasy Gra typu Game
+    while TRUE: #Pętla sprawdzająca kiedy gracz chce odejść od stołu
+        Gra.stack_creation()
+        Gra.deal_cards()
+        Gra.print_table(False)
+        move(Gra)
+        if Gra.players_list[0].passed:
+            print("Poddałeś się, gra przegrana!")
+            break
+
+        if Gra.players_list[0].bet == 0:
+            print("Gra przegrana, trzeba wejść do gry z jakąkolwiek stawką.")
+            break
+        Gra.flop = [Gra.deck.pop(), Gra.deck.pop(), Gra.deck.pop()]
+        Gra.print_table(False)
+        move(Gra)
+        if Gra.players_list[0].passed:
+            print("Poddałeś się, gra przegrana!")
+            break
+        Gra.flop.append(Gra.deck.pop())
+        Gra.print_table(False)
+        move(Gra)
+        if Gra.players_list[0].passed:
+            print("Poddałeś się, gra przegrana!")
+            break
+        Gra.flop.append(Gra.deck.pop())
+        Gra.print_table(False)
+        move(Gra)
+        if Gra.players_list[0].passed:
+            print("Poddałeś się, gra przegrana!")
+            break
+
+        Gra.print_table(True)
+        score = Gra.points()
+        print("Otrzymujesz {} punktów!".format(score))
+        input()
+    pulpit.end_pulpit()
+    
+main()
