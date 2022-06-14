d4e4b3b177977a1cd0ecf16b90224ed8a4cf860a
Aleksandra02K <101323447+Aleksandra02K@users.noreply.github.com>
2022-06-14 13:44:31 +0200

    Update game.py

--- PY_menu/game.py
+++ PY_menu/game.py
@@ -4,6 +4,11 @@ import os
 
 
 
+class Player:
+
+        def add_card(self, card):
+                self.cards.append(card)
+
 class Game:
         def stack_creation(self):
                 values = {"A": 14, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13}
